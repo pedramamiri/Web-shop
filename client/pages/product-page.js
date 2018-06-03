6522a37accef3634927c5abcd1501b06c4db683f
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2018-06-03 21:16:58 +0200

    Update product-page.js

--- client/pages/product-page.js
+++ client/pages/product-page.js
@@ -1,42 +1,56 @@
 const ProductPageComponent = {
 
-
+  props:["slice"],
 
   template: `
+    
+  <div>  
+  <input type="text" v-model="search" placeholder="sök" />
     <div class="row">
-     <product
-     v-for="product in categoryFilteredProducts"
-     v-bind:item="product"
-     v-bind:key="product._id"
-     ></product>
+    <product
+      v-for="product in categoryFilterProducts"
+      v-bind:item="product"
+      v-bind:key="product._id"
+    ></product>
     </div>
+  </div>
   `,
+
   created() {
-    http.get('/rest/products').then((response)=> {
+     // ladda in litta data
+     http.get('/rest/products/').then(response => {
+      console.log('products', response.data)
       this.products = response.data;
-    }).catch((error) => {
-      console.log(error);
+    }).catch(e => {
+      console.error(e);
     });
+
   },
+
   data() {
     return {
-      products: []
+      products: [],
+      search: ''
     }
   },
-  computed:{
-    categoryFilteredProducts: function(){
-      return this.products.filter((product)=>{
+
+  computed: {
+    filteredProducts: function() {
+      return this.products.filter((product) => {
+        return product.name.match(this.search)
+      });
+    },
+    categoryFilterProducts: function(){
+      return this.filteredProducts.filter((product)=>{
         if(!this.$route.params.category){
-          return true; // if no category selected, do not filter
+          return true;
         }
         for(let category of product.categories){
-          //console.log("category.name", category.name);
-          //console.log(" this.$route.params.category",  this.$route.params.category);
           if(category.name == this.$route.params.category){
-            return true; // found matching category
+            return true;
           }
         }
-      });
+      })
     }
   }
 }
